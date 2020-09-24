d526b21a4e79a065bf874600d32f1bd44f0eac05
Domex99 <rave.monkey36@gmail.com>
2020-09-24 14:21:58 +0200

    Interface and implementation class added

---
+++ MovieRating/MovieRating/IRatingAcces.cs
@@ -0,0 +1,12 @@
+﻿using MovieRating.BE;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieRating
+{
+    public interface IRatingAcces
+    {
+        List<Reviews> GetAll();
+    }
+}
---
+++ MovieRating/MovieRating/RatingService.cs
@@ -0,0 +1,15 @@
+﻿using MovieRating.BE;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieRating
+{
+    public class RatingService : IRatingAcces
+    {
+        public List<Reviews> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
